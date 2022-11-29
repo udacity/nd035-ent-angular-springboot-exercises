b93a16d3ce9200a9d025c4aff82cf21ecdefb868
Karen H <karen.hebel@udacity.com>
2022-11-29 13:55:18 -0500

    Remove Project

--- project/solution/src/main/java/com/udacity/store/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.udacity.store.repository;
-
-import com.udacity.store.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-    }
